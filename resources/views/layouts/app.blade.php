71ca51c8a04bafda0bc358d73d345689548be3c5
Tanvir Rahim <tanvirrajeev@gmail.com>
2020-11-28 19:01:17 +0600

    login page init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,14 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                {{-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
-                </a>
+                </a> --}}
+                <a href="{{ url('/') }}" class="brand-link">
+                    <img src="{{ asset('/img/plbd_logo.jpg') }}" alt="Powerline" class="brand-image"
+                         style="opacity: .8">
+                    <span class="brand-text font-weight-light text-dark">Powerline CPX</span>
+                  </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
